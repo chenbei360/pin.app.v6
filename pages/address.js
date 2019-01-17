f3f1e3fa0db8b71b29d47cb0f6ce4e82a055e498
3279867796 <3279867796@qq.com>
2019-01-17 22:33:50 +0800

    陈贝

--- pages/address.js
+++ pages/address.js
@@ -1,65 +1,139 @@
 // pages/address.js
+//获取应用实例
+const app = getApp(), addressEnglishNames = ['HOME', 'WORK'],addressChinaNames = ['家庭', '公司'];
+var addressId = 0, dataVersion = wx.getStorageSync('data_version'), serveDataVersion, regions = wx.getStorageSync('regions');
+
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    isLoading: true,
+    isOperate: false,
+    adTypes: addressChinaNames,
+    adTypeIdx: 0,
+    showAuthModal: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 数据版本
+  loadDataVersion: function (successCallback, failCallback) {
+    var that = this;
+    wx.request({
+      url: app.globalData.apiUrl + 'v1.0/project/data_version?type=region_list',
+      success: function (res) {
+        serveDataVersion = res.data.data_version.version;
+        successCallback();
+      },
+      fail: function (res) {
+        failCallback();
+      },
+      complete: function (res) {
+      }
+    });
+  },
+
+  laodRegions: function (successCallback, failCallback) {
+    var that = this;
+    wx.request({
+      url: app.globalData.apiUrl + 'v1.0/project/regions',
+      success: function (res) {
+        regions = res.data.regions;
+        successCallback();
+      },
+      fail: function (res) {
+        failCallback();
+      },
+      complete: function (res) {
+      }
+    });
+  },
+
+  loadAddress: function (successCallback, failCallback) {
+    
+  },
+
+  
+  loadData: function() {
+    var that = this;
+
+    app.getUserInfo(function (res) {
+      
+    }, function () {
+      that.setData({
+        showAuthModal: true,
+      });
+    });
+    
+    that.loadDataVersion(function(){
+
+      if (dataVersion != serveDataVersion){
+        that.laodRegions(function(){
+          wx.setStorageSync('data_version', serveDataVersion),
+          wx.setStorageSync('regions', regions);
+        },function(){});
+      }
+
+      var provinces = regions[1];
+      var citys = [{ "region_name": "选择城市" }];
+      var areas = [{ "region_name": "选择地区" }];
+
+      provinces.unshift({ 'region_name': '选择省份' });
+
+      that.setData({ provinces: provinces, citys: citys, areas: areas});
+
+
+      if(addressId){
+
+      }else{
+        that.setData({isLoading:false});
+      }
+
+    },function(){});
+  },
+  
   onLoad: function (options) {
+    addressId = this.options.address_id;
+    wx.setNavigationBarTitle({
+      title: addressId ? '编辑地址' : '添加新地址' 
+    })
 
+    this.loadData();
+  },
+  
+
+  openRegion: function (e) {
+    this.setData({
+      isRegionOpen: true
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+ 
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
+ 
   onShareAppMessage: function () {
 
   }
